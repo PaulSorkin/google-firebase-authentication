d42e0939d9bfe5167ec441966230ed1ee9b7747b
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-03 15:59:00 +0200

    feat: add some comments to the custom hook

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -1,10 +1,11 @@
 import {useSelector} from "react-redux";
 
+// This func is needed to determine if the user is authorized
 export function useAuth() {
     const {email, token, id} = useSelector(state => state.user);
 
     return {
-        isAuth: !!email,
+        isAuth: !!email,        //authorized user has a email
         email,
         token,
         id,
