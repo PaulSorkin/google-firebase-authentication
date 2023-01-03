46426365c591743692e37843b803d71e84579f76
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-03 23:12:03 +0200

    feat: finish with login and sign up with basic JS

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import {useDispatch} from "react-redux";
 import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
 import {Form} from "./Form";
+import {setUser} from "../store/slices/userSlice";
+import {useNavigate} from "react-router-dom";
 
 const SignUp = () => {
-    const dispatch = useDispatch()
-    const handleRegister = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleRegister = (email, password) => {
         const auth = getAuth();
         createUserWithEmailAndPassword(auth, email, password)
-            .then(console.log)
+            .then(({user}) => {
+                console.log(user)
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.accessToken,
+                }));
+                navigate('/')
+            })
             .catch(console.error)
     }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import {Navigate} from "react-router-dom";
+import {useAuth} from "../hooks/use-auth";
+import {useDispatch} from "react-redux";
+import {removeUser} from "../store/slices/userSlice";
 
 const HomePage = () => {
-    return (
+    const {isAuth, email} = useAuth();
+    const dispatch = useDispatch();
+
+    return isAuth ? (
+        <div>
+            <h1>Welcome</h1>
+
+            <button
+            onClick={() => dispatch(removeUser())}
+            >Log out from {email}</button>
+        </div>
+    ) : (
         <Navigate to="/login" />
     );
 };
